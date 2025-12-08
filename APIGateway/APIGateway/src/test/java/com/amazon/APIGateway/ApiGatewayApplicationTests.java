01a484630c9a374483685edf92816cdc94b9ef3b
Aman Jaiswal <dynamite8931p@gmail.com>
2025-12-09 00:51:19 +0530

    Implemented API gateway

---
+++ APIGateway/APIGateway/src/test/java/com/amazon/APIGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazon.APIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
